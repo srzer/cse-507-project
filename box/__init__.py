98329bf91d9b4068ea1bb09f21d8cc4a5015f637
dre <a@alipson.me>
2025-12-06 00:29:25 -0800

    add box split class to generalize splitting heuristics

--- box/__init__.py
+++ box/__init__.py
@@ -1,4 +1,21 @@
-from .type import BoxN, Point
-# from .feasibility import feasible
+from .type import BoxN, Point, from_box_model
+from .constraints import build_constraints, build_basic_box
+from .feasibility import full_check, feasible
+from .split import BoxSplit, SplitLongestSide, SplitGradient
 
-__all__ = ["BoxN", "Point"]
+__all__ = [
+    # types
+    "BoxN",
+    "Point",
+    "from_box_model",
+    # constraints
+    "build_constraints",
+    "build_basic_box",
+    # feasibility
+    "full_check",
+    "feasible",
+    # split
+    "BoxSplit",
+    "SplitLongestSide",
+    "SplitGradient",
+]
--- box/split.py
+++ box/split.py
@@ -1,21 +1,42 @@
-from typing import Tuple
+from abc import ABC, abstractmethod
+from typing import Optional, Tuple
+from typing_extensions import Self
 
-from poly import Rational
+from objective import Rational, eval_gradient
 
 from box import BoxN
-
-from poly.type import eval_gradient
+from objective.polynomial_type import unit
 
 # NOTE: rewrite these as Box methods using mixins?
 
 
 # TODO: create splitting heuristsics class to choose
 # and plug-in different splits
+class BoxSplit(ABC):
+    def _default_objective(self: Self, n: int) -> Rational:
+        return Rational(unit(n), unit(n))
+
+    def __call__(self: Self, box: BoxN, obj: Optional[Rational]) -> Tuple[BoxN, BoxN]:
+        obj = obj or self._default_objective(box.dim)
+        return self._run(box, obj)
+
+    @abstractmethod
+    def _run(self: Self, box: BoxN, obj: Rational) -> Tuple[BoxN, BoxN]: ...
+
+
+class SplitLongestSide(BoxSplit):
+    def _run(self: Self, box: BoxN, obj: Rational) -> Tuple[BoxN, BoxN]:
+        return _split_on_longest(box)
+
+
+class SplitGradient(BoxSplit):
+    def _run(self: Self, box: BoxN, obj: Rational) -> Tuple[BoxN, BoxN]:
+        return _split_by_gradient(obj, box)
 
 
 # TODO: improve function doc
 # split in direction of greatest function slope?
-def split_box_gradient(f: Rational, box: BoxN) -> Tuple[BoxN, BoxN]:
+def _split_by_gradient(f: Rational, box: BoxN) -> Tuple[BoxN, BoxN]:
     grad = eval_gradient(f, box.center)
     nonzero_gradient = any(abs(x) > 0.0 for x in grad)
     # use box interval lengths as default measure, which is the same as split on longest
@@ -29,7 +50,7 @@ def split_box_gradient(f: Rational, box: BoxN) -> Tuple[BoxN, BoxN]:
 
 
 # split on longest dimension
-def split_on_longest(box: BoxN) -> Tuple[BoxN, BoxN]:
+def _split_on_longest(box: BoxN) -> Tuple[BoxN, BoxN]:
     idx = box._max_side_idx()
     mid = box.center[idx]
 
